7a3e67f4c60988f27f3cab57182668c91ddd3e0d
djay-djou <djay_djou@hotmail.com>
2024-08-22 10:20:43 +0100

    add motivational quotes

---
+++ generatorMessage.js
@@ -0,0 +1,24 @@
+const motivationalQuotes = [
+    "The only way to do great work is to love what you do. –steve Jobs",
+    "Success is not the key to happiness. Happiness is the key to success. – Albert Schweitzer",
+    "Believe you can and you're halfway there. – Theodore Roosevelt",
+    "Your time is limited, don't waste it living someone else's life. – Steve Jobs",
+    "The harder you work for something, the greater you’ll feel when you achieve it.",
+    "Don’t stop when you’re tired. Stop when you’re done.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
+    "It does not matter how slowly you go as long as you do not stop. – Confucius",
+    "Dream it. Wish it. Do it.",
+    "Don’t watch the clock; do what it does. Keep going. – Sam Levenson",
+    "Success usually comes to those who are too busy to be looking for it. – Henry David Thoreau",
+    "The secret of getting ahead is getting started. – Mark Twain",
+    "Don’t let yesterday take up too much of today. – Will Rogers",
+    "The only limit to our realization of tomorrow is our doubts of today. – Franklin D. Roosevelt",
+    "Do something today that your future self will thank you for.",
+    "Great things never come from comfort zones.",
+    "You don’t have to be great to start, but you have to start to be great. – Zig Ziglar",
+    "The future depends on what you do today. – Mahatma Gandhi",
+    "The way to get started is to quit talking and begin doing. – Walt Disney",
+    "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle. – Christian D. Larson"
+];
+
+console.log(motivationalQuotes);
